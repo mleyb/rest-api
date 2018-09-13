322ca6316a7e09000a362f434eb0d99cd888aad8
Mark Leybourne <Mark.Leybourne@interact-intranet.com>
2018-09-13 16:34:30 +0100

    Initial version

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/people", getPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", getPerson).Methods("GET")
+	router.HandleFunc("/people/{id}", createPerson).Methods("POST")
+	router.HandleFunc("/people/{id}", deletePerson).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getPeople(w http.ResponseWriter, r *http.Request) {
+	log.Println("getPeople")
+}
+
+func getPerson(w http.ResponseWriter, r *http.Request) {
+	log.Println("getPerson")
+}
+
+func createPerson(w http.ResponseWriter, r *http.Request) {
+	log.Println("createPerson")
+}
+
+func deletePerson(w http.ResponseWriter, r *http.Request) {
+	log.Println("deletePerson")
+}
