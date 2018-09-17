03729c79c3ac62e2dbc44031cc58435b15172e23
Mark Leybourne <Mark.Leybourne@interact-intranet.com>
2018-09-17 08:53:48 +0100

    Add method bodies

--- main.go
+++ main.go
@@ -4,10 +4,29 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"surname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
 func main() {
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Y", State: "State Y"}})
+	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday", Address: &Address{City: "City Z", State: "State Z"}})
+
 	router := mux.NewRouter()
 	router.HandleFunc("/people", getPeople).Methods("GET")
 	router.HandleFunc("/people/{id}", getPerson).Methods("GET")
@@ -19,16 +38,38 @@ func main() {
 
 func getPeople(w http.ResponseWriter, r *http.Request) {
 	log.Println("getPeople")
+	json.NewEncoder(w).Encode(people)
 }
 
 func getPerson(w http.ResponseWriter, r *http.Request) {
 	log.Println("getPerson")
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(people)
 }
 
 func createPerson(w http.ResponseWriter, r *http.Request) {
 	log.Println("createPerson")
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
 }
 
 func deletePerson(w http.ResponseWriter, r *http.Request) {
-	log.Println("deletePerson")
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
 }
